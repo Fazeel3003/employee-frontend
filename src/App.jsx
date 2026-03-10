064caf4295beb6a26a8899491f6acf57ae5d5658
Fazeel <fmemon@xtsworld.in>
2026-03-10 17:11:26 +0530

    chore: add app configuration and utility functions
    - Update App.jsx with new routing and context setup
    - Add currencyFormatter utility for consistent currency handling
    - Add CURRENCY_USAGE documentation for formatter usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import { ModalProvider } from "./context/ModalContext";
+import Layout from "./layout/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import EmployeesPage from "./pages/EmployeesPage";
+import DepartmentPage from "./pages/DepartmentPage";
+import PositionsPage from "./pages/PositionsPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import EmployeeProjectsPage from "./pages/EmployeeProjectsPage";
+import AttendancePage from "./pages/AttendancePage";
+import SalaryPage from "./pages/SalaryPage";
+import LeavePage from "./pages/LeavePage";
+import ModalManager from "./components/ModalManager";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/employees" element={<EmployeesPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <ModalProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/employees" element={<EmployeesPage />} />
+            <Route path="/department" element={<DepartmentPage />} />
+            <Route path="/positions" element={<PositionsPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/employee-projects" element={<EmployeeProjectsPage />} />
+            <Route path="/attendance" element={<AttendancePage />} />
+            <Route path="/salary" element={<SalaryPage />} />
+            <Route path="/leave" element={<LeavePage />} />
+          </Routes>
+        </Layout>
+        <ModalManager />
+      </Router>
+    </ModalProvider>
   );
 }
 
